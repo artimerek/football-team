3e5637fa2235fba09831e16c1b8677c0cd454464
artimerek <konradbiznesu@gmail.com>
2022-01-13 22:15:51 +0100

    upt

--- football-team/src/main/java/pl/konradek/footballteam/FootballTeamApplication.java
+++ football-team/src/main/java/pl/konradek/footballteam/FootballTeamApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FootballTeamApplication {
 
-    //  TODO Create player validation -> name not empty longer than 1 character + ovrl in range 1 -> 99 , same with team creator
-
     public static void main(String[] args) {
         SpringApplication.run(FootballTeamApplication.class, args);
     }
